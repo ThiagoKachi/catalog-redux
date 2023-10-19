bf9d4ec6082c68a3443a0ab2f2f3248aa6921e6f
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-10-18 21:15:06 -0300

    chore: app configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      heading: ['Poppins', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          default: '#004f58',
+          hover: '#006b77',
+          active: '#00353d',
+        },
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwind-scrollbar'),
+  ],
 };
